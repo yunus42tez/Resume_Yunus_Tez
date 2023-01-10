05214f41762e1ddc412e18e7b59ab462cc8e13e6
Yunus T <Yunus T@DESKTOP-F8DOEIB>
2023-01-11 00:54:44 +0300

    Proje dosyası ekle.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Resume_Yunus_Tez.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
